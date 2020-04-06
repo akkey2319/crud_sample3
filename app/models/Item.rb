fc5023072709b58e7e251704f8d96cd8c9ffce90
akkey <akkey2319@gmail.com>
2020-04-06 20:43:38 +0900

    master

---
+++ app/models/Item.rb
@@ -0,0 +1,13 @@
+class Item
+  def self.tax_included_price(price)
+    discount_price(price) if price >= 5000
+    price *= 1.08
+    "お支払いは税込み#{price.to_i}円です”
+  end
+
+private
+  def self.discount_price(price)
+    price *= 0.9
+    binding pry
+  end
+end
